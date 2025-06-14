f8250c39e38bd159378fbe368fb801e77cf1ed03
Tvrox <yegoriyy@gmail.com>
2025-06-14 15:22:38 +0300

    add button in INDEX and add style button and add function remove modal

--- kanban_board/js/openAbout.js
+++ kanban_board/js/openAbout.js
@@ -1,5 +1,11 @@
 const about = document.querySelector('.about')
 const aboutBlock = document.querySelector('.about-block')
+const closeBtn = document.querySelector('.close-btn');
+
 about.addEventListener('click', ()=>{
     aboutBlock.classList.toggle('hide')
 })
+
+closeBtn.addEventListener('click', () => {
+    aboutBlock.classList.add('hide');
+});
